245d3340e6c1c2498d81a1e6412d60113de08ef9
E-Hussain06 <ehsanhussain991@gmail.com>
2017-09-28 12:48:56 +0100

    added jQuery logic to dropdowns

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    // Apps menu dropdown toggle
+    $("#menu-icon-apps").click(function () {
+        $("#apps-dropdown").toggleClass();
+    });
+
+    // Query Options dropdown toggle
+    $("#query-options-anchor").click(function () {
+        $("#query-options-dropdown").toggleClass();
+    });
+
+});
